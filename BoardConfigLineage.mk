e1661ce5cbb45fd3efd47bbd4013fc6fc568b46c
DhineshCool <dhineshcool585@gmail.com>
2024-03-08 18:55:37 +0530

    afterlife

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -17,7 +17,7 @@ TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/afterlife/config/device_framework_matrix.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \
@@ -28,7 +28,7 @@ endif
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Reserve space for gapps install
-ifneq ($(WITH_GMS),true)
+ifneq ($(AFTERLIFE_GAPPS),true)
 BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT := -1
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 614400000
 BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT := -1
--- afterlife_crosshatch.mk
+++ afterlife_crosshatch.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common afterlife stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
@@ -15,18 +15,16 @@ include device/google/crosshatch/crosshatch/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3 XL
-PRODUCT_NAME := lineage_crosshatch
+PRODUCT_NAME := afterlife_crosshatch
 
 # Crdroid
 TARGET_BUILD_GRAPHENEOS_CAMERA := false
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_PIXEL_DEP := true
 
 # Gapps
-WITH_GMS := true
-$(call inherit-product, vendor/gms/products/gms.mk)
+AFTERLIFE_GAPPS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=crosshatch \
